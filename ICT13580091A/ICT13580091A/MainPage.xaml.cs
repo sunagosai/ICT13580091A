53ff307ab1b7348648d77ad672a91dfdad146cb5
sopitasaisai <33734796+sopitasaisai@users.noreply.github.com>
2017-11-17 04:17:41 +0700

    testtest

---
+++ ICT13580091A/ICT13580091A/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ICT13580091A
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            okButton.Clicked += OkButton_Clicked;
+
+            genderPicker.Items.Add("ชาย");
+            genderPicker.Items.Add("หญิง");
+
+            mySlider.ValueChanged += MySlider_ValueChanged;
+
+            myStepper.ValueChanged += MyStepper_ValueChanged;
+
+        
+        }
+
+        private void MyStepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            int value = (int)e.NewValue;
+            valueLabel.Text = value.ToString();
+        }
+
+        private void MySlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            int value = (int)e.NewValue;
+            valueLabel.Text = value.ToString();
+        }
+
+        private void OkButton_Clicked(object sender, EventArgs e)
+        {
+            firstNameLabel.Text = fristNameEntry.Text;
+        }
+    }
+}
